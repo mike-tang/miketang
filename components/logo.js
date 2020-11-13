9a070e0ad2af1e1a0786f77dceae8de79618c94b
Mike Tang <michael.c.tang@gmail.com>
2020-11-12 22:58:00 -0800

    Add logo

---
+++ components/logo.js
@@ -0,0 +1,18 @@
+const Logo = () => {
+  return (
+    <svg 
+      width="100%" 
+      height="100%" 
+      viewBox="0 0 16 6" 
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M12 2L16 6H13L10.5 3.5L12 2Z" />
+      <path d="M9.5 3.5L12 6H9L8 5L9.5 3.5Z" />
+      <path d="M10 0L4 6H7L11.5 1.5L10 0Z" />
+      <path d="M6 0L0 6H3L7.5 1.5L6 0Z" />
+    </svg>
+  )
+}
+
+export default Logo
