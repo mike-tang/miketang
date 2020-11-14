0fa243c7c13e6b2dfc64f624e8a82fddad9540db
Mike Tang <michael.c.tang@gmail.com>
2020-11-14 03:14:53 -0800

    Add favicons

--- components/head.js
+++ components/head.js
@@ -19,7 +19,12 @@ const Head = ({
       <meta name="og:url" content="https://miketang.co" />
 
       {/* Favicons */}
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="manifest" href="/favicons/manifest.json" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+      <link rel="mask-icon" href="/favicons/pinned.svg" color="#111111" />
+      <meta name="theme-color" content="#111111" />
 
       {/* Other */}
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
