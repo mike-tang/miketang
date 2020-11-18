e656d7bdab1598051e1fc3ce6acf539541d48601
Mike Tang <michael.c.tang@gmail.com>
2020-11-17 21:07:30 -0800

    Update logo svg

--- components/logo.js
+++ components/logo.js
@@ -8,8 +8,7 @@ const Logo = () => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path d="M12 2L16 6H13L10.5 3.5L12 2Z" />
-      <path d="M9.5 3.5L12 6H9L8 5L9.5 3.5Z" />
-      <path d="M10 0L4 6H7L11.5 1.5L10 0Z" />
+      <path d="M7 6H4L10 0L11.5 1.5L9.5 3.5L12 6H9L8 5L7 6Z" />
       <path d="M6 0L0 6H3L7.5 1.5L6 0Z" />
     </svg>
   )
