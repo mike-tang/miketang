4cf853c8792b239837443c8532142c0f88434634
Mike Tang <michael.c.tang@gmail.com>
2022-01-15 16:18:17 -0800

    Add AudioAnimation component

---
+++ components/animation-audio.js
@@ -0,0 +1,60 @@
+/** @jsxImportSource theme-ui */
+import { keyframes } from '@emotion/react'
+
+const pulse = keyframes`
+  0% {
+		transform: scaleY(1);
+		transform-origin: 50% 50%;
+	}
+	
+	50% {
+		transform: scaleY(.64);
+		transform-origin: 50% 50%;
+	}
+	
+	100% {
+		transform: scaleY(1);
+		transform-origin: 50% 50%;
+	}
+`
+
+const AudioAnimation = ({alt, ...props}) => {
+  return (
+    <svg 
+      {...props}
+      viewBox="0 0 18 16" 
+      fill="currentColor" 
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <title>{alt}</title>
+
+      <rect x="0.5" y="6" width="1" height="4" rx="0.5" fill="currentColor" 
+        sx={{ 
+          animation: `${pulse} 1.0s 0.16s linear infinite`
+        }}
+      />
+      <rect x="4.5" y="4" width="1" height="8" rx="0.5" fill="currentColor" 
+        sx={{ 
+          animation: `${pulse} 1.0s 0.32s linear infinite`
+        }}
+      />
+      <rect x="8.5" width="1" height="16" rx="0.5" fill="currentColor" 
+        sx={{ 
+          animation: `${pulse} 1.0s 0.48s linear infinite`
+        }}
+      />
+      <rect x="12.5" y="4" width="1" height="8" rx="0.5" fill="currentColor" 
+        sx={{ 
+          animation: `${pulse} 1.0s 0.64s linear infinite`
+        }}
+      />
+      <rect x="16.5" y="6" width="1" height="4" rx="0.5" fill="currentColor" 
+        sx={{ 
+          animation: `${pulse} 1.0s 0.72s linear infinite`
+        }}
+      />
+    </svg>
+  )
+}
+
+export { AudioAnimation }
