9dc238b9199611506db8fee10bc8e3516d38a126
Mike Tang <michael.c.tang@gmail.com>
2022-01-17 02:42:41 -0800

    Add now-playing Spotify widget

---
+++ components/content/now-playing.js
@@ -0,0 +1,75 @@
+/** @jsxImportSource theme-ui */
+import { Flex, Text } from 'theme-ui'
+import { AudioAnimation } from '@/components/animation-audio'
+import useSWR from 'swr'
+import { useState, useEffect } from 'react'
+import { ListItemLink } from '@/components/links'
+
+const NowPlaying = () => {
+  const [loading, setLoading] = useState(true)
+
+  const fetcher = (url) => fetch(url).then((r) => r.json())
+  const { data } = useSWR('/api/now-playing', fetcher)
+  
+  useEffect(() => {
+    if (data) {
+      setLoading(false)
+    }
+  }, [data])
+
+  return (
+    <>
+      {/* Loading */}
+      { loading && ''}
+
+      {/* No song playing */}
+      { !loading && 
+        data &&
+        !data.isPlaying &&
+        ''
+      }
+
+      {/* Song is playing */}
+      { !loading && 
+        data?.isPlaying &&
+        
+        <Flex
+          as="section"
+          variant="layout.section"
+        >
+          <Flex
+            variant="layout.section.inner"
+          >
+            {/* Section title */}
+            <Text 
+              variant="overline"
+              pb={4}
+            >
+              Now playing
+            </Text>
+
+
+            {/* Widget */}
+            <Flex
+              sx={{
+                flexDirection: 'column',
+                gap: 4
+              }}
+            >
+              <ListItemLink 
+                icon={<AudioAnimation />}
+                url={data.songUrl}
+                textPrimary={data.title}
+                textSecondary={data.artist}
+              />
+            </Flex>
+          </Flex>
+        </Flex>
+      }
+
+    </>
+
+  )
+}
+
+export default NowPlaying
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 /** @jsxImportSource theme-ui */
 import About from '@/components/content/about'
 import Connect from '@/components/content/connect'
+import NowPlaying from '@/components/content/now-playing'
 
 const Home = () => {
   return (
     <>
       <About />
       <Connect />
-      {/* <NowPlaying /> */}
+      <NowPlaying />
     </>
   )
 }
