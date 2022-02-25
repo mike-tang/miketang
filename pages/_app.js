0efe2de81cbc3c189367bd7a7974b0e3f4659b8c
Mike Tang <michael.c.tang@gmail.com>
2022-02-24 22:47:05 -0800

    Remove Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,10 @@
 import '@/styles/global.css'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { ThemeProvider } from 'theme-ui'
 import theme from '@/styles/theme'
 import Layout from '@/components/layout'
-import { useRouter } from 'next/router'
-import * as gtag from '@/utils/gtag'
 
 const App = ({ Component, pageProps }) => {
-  
-  // Configure Google Analytics
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
 
   return (
     <ThemeProvider theme={theme}>
