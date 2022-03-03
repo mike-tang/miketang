10d54f52455045b23542b82514d2dd5cc370bd02
Mike Tang <michael.c.tang@gmail.com>
2022-03-02 19:16:50 -0800

    Rename 'song' to 'track'

--- pages/api/now-playing.js
+++ pages/api/now-playing.js
@@ -9,20 +9,20 @@ const handler = async (req, res) => {
     })
   }
 
-  const song = await response.json()
+  const track = await response.json()
 
-  if (song.item === null) {
+  if (track.item === null) {
     return res.status(200).json({ 
       isPlaying: false 
     })
   }
 
-  const isPlaying = song.is_playing
-  const title = song.item.name
-  const artist = song.item.artists.map((_artist) => _artist.name).join(', ')
-  const album = song.item.album.name
-  const albumImageUrl = song.item.album.images[0].url
-  const songUrl = song.item.external_urls.spotify
+  const isPlaying = track.is_playing
+  const title = track.item.name
+  const artist = track.item.artists.map((_artist) => _artist.name).join(', ')
+  const album = track.item.album.name
+  const albumImageUrl = track.item.album.images[0].url
+  const trackUrl = track.item.external_urls.spotify
 
   res.setHeader(
     'Cache-Control',
@@ -34,7 +34,7 @@ const handler = async (req, res) => {
     albumImageUrl,
     artist,
     isPlaying,
-    songUrl,
+    trackUrl,
     title
   })
 }
