9cee2e9f7c898c8086cc8eabae0b19b8aedc2e31
Mike Tang <michael.c.tang@gmail.com>
2022-10-30 23:29:41 -0700

    Add Vercel Analytics and enable Audiences

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,19 @@ import React from 'react'
 import { ThemeProvider } from 'theme-ui'
 import { theme } from '@/styles/theme'
 import AppLayout from '@/layouts/app'
+import { Analytics } from '@vercel/analytics/react'
 
 const App = ({ Component, pageProps }: AppProps) => {
 
   return (
-    <ThemeProvider theme={theme}>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </ThemeProvider>
+      <Analytics />
+    </>
   )
 }
 
